1971657a0982a0cfe3da153273da49d39454457f
Quasark <q@quasark.xyz>
2023-07-17 15:16:06 -0700

    1st location working

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-multi-spaces */
 'use strict';
 
+const LOCATIONS_SUMMARY = document.getElementById('locations-summary');
+
 function randomNumberGenerator(min, max) {
   let range = max - min + 1;
   let randomNumber = Math.random() * range;
@@ -9,12 +11,13 @@ function randomNumberGenerator(min, max) {
 }
 
 let seattle = {
+  locationName: 'Seattle',
   minCustomers: 23,
   maxCustomers:	65,
   avgCookiesPerCustomer:  6.3,
   hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
   generateReport: function() {
-    for (let hour of this.hours) { 
+    for (let hour of this.hours) {
       // adds a key:value object for me to reference later
       this.cookiesSoldReport[hour] = randomNumberGenerator(this.minCustomers, this.maxCustomers);
     }
@@ -24,6 +27,20 @@ let seattle = {
 seattle.cookiesSoldReport = {}; // creates an object of our daily report
 seattle.generateReport();       // fills the daily report up
 
+let newLocationElement = document.createElement('li');
+let p1 = document.createElement('p');
+let p2 = document.createElement('p');
+
+p1.innerHTML += `${seattle.locationName} details:`;
+for (let hour of seattle.hours) {
+  let newString = `${hour}: ${seattle.cookiesSoldReport[hour]} cookies<br>`;
+  p2.innerHTML += newString;
+}
+
+newLocationElement.appendChild(p1);
+newLocationElement.appendChild(p2);
+
+LOCATIONS_SUMMARY.appendChild(newLocationElement);
 
 
-console.log(seattle.arrayExample);
+
