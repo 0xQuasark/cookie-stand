e505816bb9fbaff7fdb4c1cff271c6490f86d0e5
Quasark <q@quasark.xyz>
2023-07-17 15:46:11 -0700

    approaching final submission for the day

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ function randomNumberGenerator(min, max) {
   return Math.floor(randomNumber);
 }
 
+// LOCATION: SEATTLE
 let seattle = {
   locationName: 'Seattle',
   minCustomers: 23,
@@ -42,5 +43,167 @@ newLocationElement.appendChild(p2);
 
 LOCATIONS_SUMMARY.appendChild(newLocationElement);
 
+// ----------------------------------------------------------
+// End of Seattle
+// ----------------------------------------------------------
 
+
+// ----------------------------------------------------------
+// LOCATION: tokyo
+// ----------------------------------------------------------
+let tokyo = {
+  locationName: 'Tokyo',
+  minCustomers: 23,
+  maxCustomers:	65,
+  avgCookiesPerCustomer:  6.3,
+  hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+  generateReport: function() {
+    for (let hour of this.hours) {
+      // adds a key:value object for me to reference later
+      this.cookiesSoldReport[hour] = randomNumberGenerator(this.minCustomers, this.maxCustomers);
+    }
+  }
+};
+
+tokyo.cookiesSoldReport = {}; // creates an object of our daily report
+tokyo.generateReport();       // fills the daily report up
+
+newLocationElement = document.createElement('li');
+p1 = document.createElement('p');
+p2 = document.createElement('p');
+
+p1.innerHTML += `${tokyo.locationName} details:`;
+for (let hour of tokyo.hours) {
+  let newString = `${hour}: ${tokyo.cookiesSoldReport[hour]} cookies<br>`;
+  p2.innerHTML += newString;
+}
+
+newLocationElement.appendChild(p1);
+newLocationElement.appendChild(p2);
+
+LOCATIONS_SUMMARY.appendChild(newLocationElement);
+
+// ----------------------------------------------------------
+// End of tokyo
+// ----------------------------------------------------------
+
+
+// ----------------------------------------------------------
+// LOCATION: dubai
+// ----------------------------------------------------------
+let dubai = {
+  locationName: 'Dubai',
+  minCustomers: 23,
+  maxCustomers:	65,
+  avgCookiesPerCustomer:  6.3,
+  hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+  generateReport: function() {
+    for (let hour of this.hours) {
+      // adds a key:value object for me to reference later
+      this.cookiesSoldReport[hour] = randomNumberGenerator(this.minCustomers, this.maxCustomers);
+    }
+  }
+};
+
+dubai.cookiesSoldReport = {}; // creates an object of our daily report
+dubai.generateReport();       // fills the daily report up
+
+newLocationElement = document.createElement('li');
+p1 = document.createElement('p');
+p2 = document.createElement('p');
+
+p1.innerHTML += `${dubai.locationName} details:`;
+for (let hour of dubai.hours) {
+  let newString = `${hour}: ${dubai.cookiesSoldReport[hour]} cookies<br>`;
+  p2.innerHTML += newString;
+}
+
+newLocationElement.appendChild(p1);
+newLocationElement.appendChild(p2);
+
+LOCATIONS_SUMMARY.appendChild(newLocationElement);
+
+// ----------------------------------------------------------
+// End of dubai
+// ----------------------------------------------------------
+
+
+// ----------------------------------------------------------
+// LOCATION: paris
+// ----------------------------------------------------------
+let paris = {
+  locationName: 'Paris',
+  minCustomers: 23,
+  maxCustomers:	65,
+  avgCookiesPerCustomer:  6.3,
+  hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+  generateReport: function() {
+    for (let hour of this.hours) {
+      // adds a key:value object for me to reference later
+      this.cookiesSoldReport[hour] = randomNumberGenerator(this.minCustomers, this.maxCustomers);
+    }
+  }
+};
+
+paris.cookiesSoldReport = {}; // creates an object of our daily report
+paris.generateReport();       // fills the daily report up
+
+newLocationElement = document.createElement('li');
+p1 = document.createElement('p');
+p2 = document.createElement('p');
+
+p1.innerHTML += `${paris.locationName} details:`;
+for (let hour of paris.hours) {
+  let newString = `${hour}: ${paris.cookiesSoldReport[hour]} cookies<br>`;
+  p2.innerHTML += newString;
+}
+
+newLocationElement.appendChild(p1);
+newLocationElement.appendChild(p2);
+
+LOCATIONS_SUMMARY.appendChild(newLocationElement);
+
+// ----------------------------------------------------------
+// End of paris
+// ----------------------------------------------------------
+
+
+// ----------------------------------------------------------
+// LOCATION: lima
+// ----------------------------------------------------------
+let lima = {
+  locationName: 'Lima',
+  minCustomers: 23,
+  maxCustomers:	65,
+  avgCookiesPerCustomer:  6.3,
+  hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+  generateReport: function() {
+    for (let hour of this.hours) {
+      // adds a key:value object for me to reference later
+      this.cookiesSoldReport[hour] = randomNumberGenerator(this.minCustomers, this.maxCustomers);
+    }
+  }
+};
+
+lima.cookiesSoldReport = {}; // creates an object of our daily report
+lima.generateReport();       // fills the daily report up
+
+newLocationElement = document.createElement('li');
+p1 = document.createElement('p');
+p2 = document.createElement('p');
+
+p1.innerHTML += `${lima.locationName} details:`;
+for (let hour of lima.hours) {
+  let newString = `${hour}: ${lima.cookiesSoldReport[hour]} cookies<br>`;
+  p2.innerHTML += newString;
+}
+
+newLocationElement.appendChild(p1);
+newLocationElement.appendChild(p2);
+
+LOCATIONS_SUMMARY.appendChild(newLocationElement);
+
+// ----------------------------------------------------------
+// End of lima
+// ----------------------------------------------------------
 
